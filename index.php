68eaccc64dde03093959927a9d702df198e4da16
Rios2002S <rios222002@gmail.com>
2024-12-17 15:24:44 -0600

    Actualización de Login
    Terminado

--- index.php
+++ index.php
@@ -1,41 +1,90 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tienda</title>
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <!-- Estilos CSS -->
-    <link rel="stylesheet" href="css/estilos.css">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+	<title>Login V2</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/principal.css">
+<!--===============================================================================================-->
 </head>
 <body>
-<?php
-if (isset($_GET['mensaje'])) {
-    $mensaje = urldecode($_GET['mensaje']);
-    echo '<script>alert("' . $mensaje . '");</script>';
-}
-?>
-    <main>
-        <article>
-            <section>
-                <form action="./bd/verifyuser.php" method="POST">
-                    <h1>Inicia Sesion</h1>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form  method="POST" action="bd/verifyuser.php" class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Bienvenido
+					</span>
+					<span class="login100-form-title p-b-48">
+					<i class="fas fa-user"></i>
+					</span>
 
-                    <input type="text" name="nombreusu" placeholder="Usuario"><br/>
-                    <input type="password" name="contrasena" placeholder="Contrase&ntilde;a" required><br/>
-                    <button type="submit">Entrar</button>
-                    <button type="reset">Limpiar</button>
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="nombreusu" required>
+						<span class="focus-input100" data-placeholder="Nombre de Usuario"></span>
+					</div>
 
-                    <p>Si no posees cuenta pide al administrador que te asigne una</p>
+					<div class="wrap-input100 validate-input" data-validate="Ingrese su contraseña">
+						<span class="btn-show-pass">
+						<i class="fas fa-eye"></i>
+						</span>
+						<input class="input100" type="password" name="contrasena">
+						<span class="focus-input100" data-placeholder="Contraeña"></span>
+					</div>
 
-                </form>
-            </section>
-        </article>
-    </main>
-    
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Iniciar Sesiòn
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
